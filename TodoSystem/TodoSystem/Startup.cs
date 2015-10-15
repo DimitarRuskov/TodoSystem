722f9e7e43d6b9e98ebdafd50ea0a591e492d3aa
Dimitar Ruskov <dimitar.ruskov@softwaregroup-bg.com>
2015-10-15 17:29:28 +0300

    ASP.Net starter pack with microsoft user management

---
+++ TodoSystem/TodoSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TodoSystem.Startup))]
+namespace TodoSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
