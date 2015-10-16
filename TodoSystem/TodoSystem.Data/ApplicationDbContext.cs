95b81e7b53eeda0db891022179ad395dadd3b1c2
Marushkov <george.marushkov@gmail.com>
2015-10-16 22:47:56 +0300

    entity framework setup and user management included

---
+++ TodoSystem/TodoSystem.Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using TodoSystem.Models;
+
+namespace TodoSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ TodoSystem/TodoSystem.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TodoSystem.Web.Startup))]
+namespace TodoSystem.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
