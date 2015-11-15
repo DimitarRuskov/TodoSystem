5e70df977f5fc98b774efd8f9d8ef0c8cab182d1
DimitarRuskov <dimi7ar.ru5kov@gmail.com>
2015-11-15 10:40:38 +0200

    authorization

--- Bookmarks/Bookmarks.Web/Models/AccountViewModels.cs
+++ Bookmarks/Bookmarks.Web/Models/AccountViewModels.cs
@@ -73,6 +73,14 @@ namespace Issues.Web.Models
         public string UserName { get; set; }
 
         [Required]
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
